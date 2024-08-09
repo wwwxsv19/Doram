c585d16b8b5c1d2bdf25997e1472bdf1eb3c14b5
wwwxsv19 <wwwxsv19@gmail.com>
2024-08-09 23:27:58 +0900

    set : project

---
+++ src/main/java/bssm/devcoop/DoramServerApplication.java
@@ -0,0 +1,13 @@
+package bssm.devcoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoramServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoramServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bssm/devcoop/DoramServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bssm.devcoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoramServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
